5b5619a186ffc8874d9ba2a819e23608583a98bc
Leonardo Hirotaka Oka <leookajp99@hotmail.com>
2020-03-25 23:01:35 -0300

    first commit

---
+++ Assets/spawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+    float tiltAngle = 60.0f; 
+    public GameObject prefab;
+    public float tempo = 0;
+	public float novaRotation;
+
+    // private Transform target;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        tempo += Time.deltaTime;
+
+        if(tempo > 5)
+		{
+            prefab.transform.position = new Vector3(Random.Range(-5,5),Random.Range(-5,5), 0);
+            prefab.transform.rotation = Quaternion.Euler(0, 0, Random.Range(-180, 180));
+            Instantiate(prefab);
+            
+            tempo = 0;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# allan-trabalho-aula6
